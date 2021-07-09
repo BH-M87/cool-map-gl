97978e82588bb96ce2dcdc168d6d08a97edaa062
Xuheng Liu <liuxuheng1990@gmail.com>
2021-07-10 01:20:59 +0800

    chore: ts-nocheck

--- src/layers/ClusterLayer.ts
+++ src/layers/ClusterLayer.ts
@@ -3,78 +3,86 @@
  * @Date: 2021-07-08 19:45:34
  * @LastEditors: Mochi
  * @LastEditTime: 2021-07-10 00:51:31
- * @Description: 
+ * @Description:
  */
+// @ts-nocheck
+(function () {
+  'use strict';
 
-// @ts-ignore
-(function() {
-    'use strict';
-  
-    // save the original methods before overwriting them
-    Element.prototype._addEventListener = Element.prototype.addEventListener;
-    Element.prototype._removeEventListener = Element.prototype.removeEventListener;
-  
-    /**
-     * [addEventListener description]
-     * @param {[type]}  type       [description]
-     * @param {[type]}  listener   [description]
-     * @param {Boolean} useCapture [description]
-     */
-    Element.prototype.addEventListener = function(type,listener,useCapture=false) {
-        // declare listener
-        this._addEventListener(type,listener,useCapture);
-  
-        if(!this.eventListenerList) this.eventListenerList = {};
-        if(!this.eventListenerList[type]) this.eventListenerList[type] = [];
-  
-        // add listener to  event tracking list
-        this.eventListenerList[type].push( {type, listener, useCapture} );
-    };
-  
-    /**
-     * [removeEventListener description]
-     * @param  {[type]}  type       [description]
-     * @param  {[type]}  listener   [description]
-     * @param  {Boolean} useCapture [description]
-     * @return {[type]}             [description]
-     */
-    Element.prototype.removeEventListener = function(type,listener,useCapture=false) {
-        // remove listener
-        this._removeEventListener(type,listener,useCapture);
-  
-        if(!this.eventListenerList) this.eventListenerList = {};
-        if(!this.eventListenerList[type]) this.eventListenerList[type] = [];
-  
-        // Find the event in the list, If a listener is registered twice, one
-        // with capture and one without, remove each one separately. Removal of
-        // a capturing listener does not affect a non-capturing version of the
-        // same listener, and vice versa.
-        for(let i=0; i<this.eventListenerList[type].length; i++){
-            if( this.eventListenerList[type][i].listener===listener && this.eventListenerList[type][i].useCapture===useCapture){
-                this.eventListenerList[type].splice(i, 1);
-                break;
-            }
-        }
-        // if no more events of the removed event type are left,remove the group
-        if(this.eventListenerList[type].length==0) delete this.eventListenerList[type];
-    };
-  
-  
-    /**
-     * [getEventListeners description]
-     * @param  {[type]} type [description]
-     * @return {[type]}      [description]
-     */
-    Element.prototype.getEventListeners = function(type){
-        if(!this.eventListenerList) this.eventListenerList = {};
-  
-        // return reqested listeners type or all them
-        if(type===undefined)  return this.eventListenerList;
-        return this.eventListenerList[type];
-    };
-  
-  
-    /*
+  // save the original methods before overwriting them
+  Element.prototype._addEventListener = Element.prototype.addEventListener;
+  Element.prototype._removeEventListener = Element.prototype.removeEventListener;
+
+  /**
+   * [addEventListener description]
+   * @param {[type]}  type       [description]
+   * @param {[type]}  listener   [description]
+   * @param {Boolean} useCapture [description]
+   */
+  Element.prototype.addEventListener = function (
+    type: string | number,
+    listener: (this: Element, ev: any) => any,
+    useCapture = false,
+  ) {
+    // declare listener
+    this._addEventListener(type, listener, useCapture);
+
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
+
+    // add listener to  event tracking list
+    this.eventListenerList[type].push({ type, listener, useCapture });
+  };
+
+  /**
+   * [removeEventListener description]
+   * @param  {[type]}  type       [description]
+   * @param  {[type]}  listener   [description]
+   * @param  {Boolean} useCapture [description]
+   * @return {[type]}             [description]
+   */
+  Element.prototype.removeEventListener = function (
+    type: string | number,
+    listener: (this: Element, ev: any) => any,
+    useCapture = false,
+  ) {
+    // remove listener
+    this._removeEventListener(type, listener, useCapture);
+
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
+
+    // Find the event in the list, If a listener is registered twice, one
+    // with capture and one without, remove each one separately. Removal of
+    // a capturing listener does not affect a non-capturing version of the
+    // same listener, and vice versa.
+    for (let i = 0; i < this.eventListenerList[type].length; i++) {
+      if (
+        this.eventListenerList[type][i].listener === listener &&
+        this.eventListenerList[type][i].useCapture === useCapture
+      ) {
+        this.eventListenerList[type].splice(i, 1);
+        break;
+      }
+    }
+    // if no more events of the removed event type are left,remove the group
+    if (this.eventListenerList[type].length == 0) delete this.eventListenerList[type];
+  };
+
+  /**
+   * [getEventListeners description]
+   * @param  {[type]} type [description]
+   * @return {[type]}      [description]
+   */
+  Element.prototype.getEventListeners = function (type) {
+    if (!this.eventListenerList) this.eventListenerList = {};
+
+    // return reqested listeners type or all them
+    if (type === undefined) return this.eventListenerList;
+    return this.eventListenerList[type];
+  };
+
+  /*
     Element.prototype.clearEventListeners = function(a){
         if(!this.eventListenerList)
             this.eventListenerList = {};
@@ -91,7 +99,6 @@
         }
     };
     */
-  
 })();
 export class ClusterLayer {
   id: any;
@@ -386,24 +393,24 @@ export class ClusterLayer {
       map.style.glyphManager._tinySDF.rewrite = true;
     }
 
-
     const node = map.getContainer().parentNode.parentNode;
-    node.getEventListeners().click&&node.getEventListeners().click.forEach((e: any)=>{
-        node.removeEventListener('click',e.listener);
-    })
-    node.addEventListener("click", (e:any) => {
-        // this.drill({
-        //     x:e.offsetX,
-        //     y:e.offsetY
-        // })
-        this.drill([e.offsetX,e.offsetY]);
-        let feature = this.getFeature([e.offsetX,e.offsetY]);
-        if(feature){
-            this.onIconClick({
-                object:feature.properties,
-                event:e
-            });
-        }
+    node.getEventListeners().click &&
+      node.getEventListeners().click.forEach((e: any) => {
+        node.removeEventListener('click', e.listener);
+      });
+    node.addEventListener('click', (e: any) => {
+      // this.drill({
+      //     x:e.offsetX,
+      //     y:e.offsetY
+      // })
+      this.drill([e.offsetX, e.offsetY]);
+      let feature = this.getFeature([e.offsetX, e.offsetY]);
+      if (feature) {
+        this.onIconClick({
+          object: feature.properties,
+          event: e,
+        });
+      }
     });
 
     return this.setData(this.data).then(() => {
